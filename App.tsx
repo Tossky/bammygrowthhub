2a74fc4ef6507d218d0c8e201375ec5612c9f1e0
Tossky <104205098+Tossky@users.noreply.github.com>
2025-09-30 02:02:33 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import SeoVisibilityPage from './pages/SeoVisibilityPage';
 import DigitalMarketingPage from './pages/DigitalMarketingPage';
